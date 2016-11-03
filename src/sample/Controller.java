20c40e036970be2d8ce242c488950a8407991436
Oscar <sanmoroscar@gmail.com>
2016-11-03 18:11:44 +0100

    Font manager

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,16 +3,28 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.text.Font;
 
 
 public class Controller {
     @FXML
     private TextArea txtArea;
-
+    @FXML
+    private CheckMenuItem Arial;
+    @FXML
+    private CheckMenuItem TimesNewRoman;
+    @FXML
+    private CheckMenuItem Size24;
+    @FXML
+    private CheckMenuItem Size11;
 
     public void onClick(ActionEvent e) {
 
         //Creamos un action event para el menu
+        CheckMenuItem fonts[] = {Arial, TimesNewRoman};
+        CheckMenuItem size[] = {Size24, Size11};
+        double tSize = txtArea.getFont().getSize();
+        String tFont = txtArea.getFont().getFamily();
         Button tempBton;
         MenuItem tempMenuItem;
         MenuItem tempCheckMenu;
@@ -35,9 +47,32 @@ public class Controller {
                 txtArea.copy();
                 txtArea.requestFocus();
                 break;
+            case "Cortar":
+                txtArea.cut();
+                txtArea.requestFocus();
+                break;
             case "Pegar":
                 txtArea.paste();
                 txtArea.requestFocus();
+                break;
+
+            case "Arial":
+                txtArea.setFont(Font.font("Arial", tSize));
+                for (CheckMenuItem item : fonts) {
+                    if (!item.getId().equalsIgnoreCase("Arial") && item.isSelected()) {
+                        item.setSelected(false);
+                        return;
+                    }
+                }
+                break;
+            case "TimesNewRoman":
+                txtArea.setFont(Font.font("TimesNewRoman", tSize));
+                for (CheckMenuItem item : fonts) {
+                    if (!item.getId().equalsIgnoreCase("TimesNewRoman") && item.isSelected()) {
+                        item.setSelected(false);
+                        return;
+                    }
+                }
                 break;
             case "App":
                 Alert info = new Alert(Alert.AlertType.INFORMATION);
@@ -46,5 +81,6 @@ public class Controller {
                 info.setHeaderText("JavaFX P03");
                 info.show();
         }
+
     }
 }
